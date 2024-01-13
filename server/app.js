7352824d008536439190a8be71400fbcf5ede928
Pritam Kamble <pritam.kamble@linkcode.in>
2024-01-13 14:34:38 +0530

    db added

--- server/app.js
+++ server/app.js
@@ -1,4 +1,14 @@
 var express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB).then(() => {
+  console.log('Database connection established');
+}).catch((error) => {
+  console.log(error);
+  console.log('Error connecting to Mongo');
+});
+
 var usersRouter = require('./api/user/user.route');
 
 var app = express();
